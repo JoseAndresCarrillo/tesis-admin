90f91f18e5af7720014ca7c346fcdf0c6861c091
José Andrés Carrillo Estrada <jace796@gmail.com>
2018-10-08 17:38:57 -0500

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,99 @@
+require('dotenv').config()
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // DotEnv
+    '@nuxtjs/dotenv',
+
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios',
+
+    // Doc: https://auth.nuxtjs.org/
+    '@nuxtjs/auth'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+    // Local Auth module
+
+  auth: {
+    redirect: {
+      login: '/',
+      logout: '/',
+      home: '/dashboard/'
+    },
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/admin/auth/login', method: 'post', propertyName: 'token' },
+          logout: false,
+          user: { url: '/admin/account', method: 'get', propertyName: 'user' }
+        }
+      }
+    }
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
